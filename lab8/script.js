b2b7fcec96f9d152d0c8c268b57a40f32f468955
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2023-02-19 14:05:05 +0300

    updated lab8

--- lab8/script.js
+++ lab8/script.js
@@ -1,31 +1,55 @@
 let videoElem = document.getElementById("video");
-let playButton = document.getElementById("playbutton");
+let playButton = document.getElementById("play-video");
+let areaPolyUp = document.getElementById("poly-up");
+let areaPolyBottom = document.getElementById("poly-bot");
 
-playButton.addEventListener("click", handlePlayButton, false);
+areaPolyUp.coords = createUpperPoints(300, 300, 190, 190, 100, 0.1);
+areaPolyBottom.coords = createBottomPoints(300, 300, 190, 190, 100, 0.1);
 
-playVideo();
 
 async function playVideo() {
-    console.log("playing");
     try {
         await videoElem.play();
-        playButton.className = "playing";
+        console.log("playing...")
     } catch(err) {
-        playButton.className = "";
+        console.error(err)
     }
 }
 
 function handlePlayButton() {
     if (videoElem.paused) {
         playVideo();
+        playButton.innerText = "Stop";
     } else {
         videoElem.pause();
-        playButton.className = "";
-        console.log("stop")
+        playButton.innerText = "Play";
+        console.log("stopped");
     }
 }
 
-function play() {
+function playAudio() {
     var audio = document.getElementById("audio");
     audio.play();
+}
+
+function createUpperPoints(w, h, cx, cy, r, dt){
+    let res = [`0,0`, `0,${cy}`];
+
+    for (let t = 0.0; t >= -Math.PI; t -= dt){
+        res.push((Math.floor(cx + Math.cos(t + Math.PI)*r)) + ',' + (Math.floor(cy + Math.sin(t)*r)));
+    }
+    res.push(`${w},${cy}`)
+    res.push(`${w},0`)
+    return res.join(' ')
+}
+
+function createBottomPoints(w, h, cx, cy, r, dt){
+    let res = [`0,${h}`, `0,${cy}`];
+
+    for (let t = 0.0; t >= -Math.PI; t -= dt){
+        res.push((Math.floor(cx + Math.cos(Math.PI - t)*r)) + ',' + (Math.floor(cy + Math.sin(-t)*r)));
+    }
+    res.push(`${w},${cy}`)
+    res.push(`${w},${h}`)
+    return res.join(' ')
 }
